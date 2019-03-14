ad5aea9e31c1303643edc74d4e89adf802a632bd
BenRauzi <46276310+BenRauzi@users.noreply.github.com>
2019-03-15 08:04:38 +1300

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,121 +1,176 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 import random
 
-def restart():
-	root.destroy()
-	start()
+def init():
+	global var, label, root
 
-def start():
-	global root, col, bombCount, bombPositions, button,flagged
-	col = 10
-	bombCount = 20
-	bombPositions=[]
-	button = list()
-	flagged=[]
-	root = tk.Tk()
+	root= Tk()
 	root.title("Minesweeper")
-	root.resizable(0, 0)
-	del bombPositions[:]
-	frame = tk.Frame(root)
-	frame.pack()
+	root.geometry("250x65+500+100")
+	root.resizable(0,0)
 
-	new = tk.Button(frame, text="RESTART", fg="red",width=53, height=1,command=restart)
-	new.pack(side=tk.LEFT)
+	label = Label(root, text="Enter the grid size:")
+	label.pack()
 
-	frame1 = tk.Frame(root)
-	frame1.pack(side=tk.TOP, fill=tk.X)
-	buttonCount = []
-	for y in range(col):
-		for x in range(col):
-			button.append(tk.Button(frame1,width=4,height=2, command=lambda x=x, y=y: onClick(x, y)))
-			button[-1].bind('<Button-3>', lambda z=1, x=x, y=y: onRightClick(x,y)) #z=1 for some reason requited, or lambda function argument recieves binding stuff
-			button[-1].grid(row=y,column=x)
-			buttonCount.append((y*10)+x)
-	createBombs()
+	var = StringVar()
+	entry = ttk.Entry(root, textvariable=var, width=40)
+	entry.pack()
 
+	enterButton = ttk.Button(root, width=50, text="Start", command=tryStart)
+	enterButton.pack()
 
 	root.mainloop()
 
-def onRightClick(x,y):
-
-	i = (10*y)+x
-
-	if not ((button[i])["text"]) == "Flag":
-		(button[i])["text"] = "Flag"
-		(button[i])["state"]="disabled"
-		flagged.append(i)
-		flagged.sort()
-
-		if flagged == bombPositions:
-			end(1)
-
-	else:
-		(button[i])["state"]="active"
-		(button[i])["text"] = ""
-		flagged.remove(i)
-
-def onClick(x, y):
-	if not end == 1:
-		i = (10*y)+x
-		print("%sx %dy, position %s" % (x,y, i))
-
-		if i in bombPositions:
-			end(0)
-			(button[i])["text"] = "Bomb"
+def tryStart():
+	global cols
+	try:
+		cols = int(var.get())
+		if (cols < 100):
+			root.destroy()		
+			start(cols)
 		else:
-			params1 = [i-col-1,i-col,i-col+1,i-1,i+1,i+col-1,i+col,i+col+1]
-			params2 = [i-col,i-col+1,i+1,i+col,i+col+1]
-			params3 = [i-col-1,i-col,i-1,i+col-1,i+col]
-			
-			if not(x==0 or x==9):
-				params=params1
-			elif x==0:
-				params=params2
-			elif x==9:
-				params=params3
-
-			bombs=0
-			for u in params:
-				if u in bombPositions:
-					bombs+=1
-
-			(button[i])["text"] = bombs
+			print("Not a valid input")
+			label["text"] = "Please Enter a Valid Input"
+			var.set("")
+	except ValueError:
+		print("Not a valid input")
+		label["text"] = "Please Enter a Valid Input"
+		var.set("")
 
 
-def createBombs():
-	for x in range(bombCount):
-		bombPositions.append(random.randint(0,col**2))
+def start(cols):
+	global mainRoot, arr, bombs, revealedButtons, flagPositions
+	mainRoot = Tk()
+	mainRoot.title("Minesweeper")
 
-	bombPositions.sort()
-def end(win):
-	for y in range(col):
-		for x in range(col):
-			i = (10*y)+x
-			(button[i])["state"]="disabled"
-			if i in bombPositions:
-				(button[i])["text"] = "Bomb"
-			else:
-				params1 = [i-col-1,i-col,i-col+1,i-1,i+1,i+col-1,i+col,i+col+1]
-				params2 = [i-col,i-col+1,i+1,i+col,i+col+1]
-				params3 = [i-col-1,i-col,i-1,i+col-1,i+col]
-				
-				if not(x==0 or x==9):
-					params=params1 
-				elif x==0:
-					params=params2
-				elif x==9:
-					params=params3
 
-				bombs=0
-				for u in params:
-					if u in bombPositions:
-						bombs+=1
+	master = LabelFrame(mainRoot, text="Minesweeper")
+	master.pack()
 
-				(button[i])["text"] = bombs
+	frameTop = Frame(master)
+	frameTop.pack()
 
-	if win == 1:
-		print("Win")
+	topButton = ttk.Button(frameTop, text="Restart", width=int(5.5*cols), command=restart)
+	topButton.pack()
+
+	frameMain = Frame(master)
+	frameMain.pack()
+
+	flagPositions = []
+	arr = []
+	bombs = []
+	for x in range(cols):
+		for y in range(cols):
+			arr.append(ttk.Button(frameMain,width=4, text="{}, {}".format(x,y), command=lambda x=x, y=y: onClick(x,y)))
+			arr[-1].bind("<Button-3>", lambda z=1, x=x, y=y: onRightClick(z,x,y))
+			arr[-1].grid(row=x, column=y)
+
+
+			if random.randint(0,cols) < cols/5:
+				if 	x == 0 or y == 0 or x*y in bombs:
+					continue
+				bombs.append(x*y)
+	bombs.sort()
+	print(bombs)
+
+
+	revealedButtons = []
+	mainRoot.resizable(0,0)
+	mainRoot.mainloop()
+
+def onClick(x,y):
+	print("Debug --", x,y, y + x * cols)
+
+	position = y + x * cols
+	if position in bombs:
+		end(False)
+		arr[position]["text"] = "Bomb"
 	else:
-		print("Loss")
+		reveal(position, x, y)
+def onRightClick(this,x,y):
+	position = y + x * cols
 
-start()
+	print(x,y, position)
+
+	if not position in flagPositions:
+		flagPositions.append(position)
+		arr[position]["text"] = "Flag"
+	else:
+		flagPositions.remove(position)
+		arr[position]["text"] = "{}, {}".format(x,y)
+
+	print(flagPositions)
+	print(bombs)
+	flagPositions.sort()
+	if flagPositions == bombs:
+		print("Win")
+		end(True)
+
+def reveal(args ,x ,y):
+
+	if args in revealedButtons:
+		return 0;
+	revealedButtons.append(args)
+
+	if x==0:
+		params=[args-cols,args-cols+1,args+1,args+cols,args+cols+1]
+	elif x==9:
+		params=[args-cols-1,args-cols,args-1,args+cols-1,args+cols]
+	else:
+		params= [args-cols-1,args-cols,args-cols+1,args-1,args+1,args+cols-1,args+cols,args+cols+1]
+	
+	nearBombs=0
+	for i in params:
+		if i in bombs and i > 0 and i < cols**2:
+			nearBombs+=1
+
+	arr[args]["text"] = nearBombs
+
+	if nearBombs == 0:
+		for i in [args-cols,args-1,args+1,args+cols]:
+			try:
+				if i < 10 and i > 0:
+					reveal(i, 0, str(i))
+				if i < 100 and i > 0 and i < cols**2:
+					reveal(i, str(i)[0], str(i)[1])
+				elif i > 0 and i < cols**2:
+					reveal(i, str(i)[0:2], str(i)[2])
+			except IndexError:
+				pass
+
+def end(cond):
+	print("Game Over")
+	for i in arr:
+
+		i["state"] = "disabled"
+
+def restart():
+	mainRoot.destroy()
+	init()
+
+def scoreBoard():
+	scoreBoard = Tk()
+	scoreBoard.title("High Scores")
+	scoreBoard.resizable(0,0)
+
+	title = ttk.Label(scoreBoard, text="High Scores")
+	title.pack(pady=5)
+
+	frameMain = Frame(scoreBoard)
+	frameMain.pack()
+
+	highScoreList = Listbox(scoreBoard, width=50)
+	highScoreList.pack()
+
+	highScoreList.insert(END,"Test")
+
+	highScoreList.insert(END,"Test2")
+
+	scoreBoard.mainloop()
+
+def addScore():
+	pass
+init()
+#scoreBoard()
+
